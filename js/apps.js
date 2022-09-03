d9ebac316b341e765f2d95c6158abc1589fc6a82
Marfater Rahman Jabed <marfaterrahman@gmail.com>
2022-09-03 16:54:38 +0600

    icon added

--- js/apps.js
+++ js/apps.js
@@ -6,20 +6,70 @@ const LoadApi = () => {
 }
 
 const displayApi = (data) => {
-    console.log(data);
+    // console.log(data);
     const catagoriList = document.getElementById('catagory-List');
     catagoriList.classList.add('mx-4');
     data.forEach(item => {
         const text = document.createElement('span');
         // text.innerText = item.category_name;
         text.innerHTML = `
-        <span class="mx-3 "><a href="#" class="text-decoration-none text-secondary">${item.category_name}</a></span>
+        <span class="mx-3 "><a href="#" class="text-decoration-none text-secondary" onclick ="catagories(data)" id="${item.category_id}">${item.category_name}</a></span>
         `
 
         catagoriList.appendChild(text);
+
     });
-
-
 }
 
+const catagories = () => {
+    fetch(`https://openapi.programming-hero.com/api/news/category/01`)
+        .then(res => res.json())
+        .then(data => DisplayCategories(data.data))
+}
+
+const DisplayCategories = (data) => {
+    console.log(data);
+    const cardSection = document.getElementById('card-section');
+
+
+    data.forEach(item => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.classList.add('mb-3');
+        div.innerHTML = `
+        <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${item.image_url}" class="img-fluid rounded-start"  alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h6 class="card-title text-dark">${item.title}</h6>
+                            <p class="card-text">${item.details.slice(0, 200)}...</p>
+                            <div class="d-flex justify-content-between">
+                            <div>
+                            <small><img src="${item.author.img}" width='50px' class="rounded-circle"></small>
+                           
+                           <small class="">${item.author.name}</small>
+                            <small>${item.author.published_date}</small>
+                           
+                            
+                            </div> 
+                            <div>
+                            <small><i class="fa-solid fa-eye"></i></small>
+                            <small>${item.total_view}</small><small></small>
+                            </div>
+                            <div>
+                            <i class="fa-solid fa-arrow-right"></i>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        `;
+        cardSection.appendChild(div);
+
+    })
+}
+
+catagories();
 LoadApi();
